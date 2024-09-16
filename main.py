93f01d1ed04ae447c55a1549a4a4e1a0e9e02ba3
VIGNESHWAR REDDY VURAGAYALA <127202144+Vignesh-war-Reddy@users.noreply.github.com>
2024-09-16 15:55:51 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 import random
 
-num = random.randrange(1000, 10000)
-n = int(input("Guess the 4-digit number:"))
+print("Hi welcome to the game, This is a number guessing game.\nYou got 7 chances to guess the number. Let's start the game")
 
-if n == num:
-    print("Great! You guessed the number in just 1 try! You're a Mastermind!")
-else:
-    ctr = 0
+number_to_guess = random.randrange(100)
 
-    while n != num:
-        ctr += 1
-        count = 0
+chances = 7
 
-        n_str = str(n)
-        num_str = str(num)
+guess_counter = 0
 
-        for i in range(4):
-            if n_str[i] == num_str[i]:
-                count += 1
+while guess_counter < chances:
 
-        if count > 0:
-            print(f"Not quite the number. But you got {count} digit(s) correct!")
-        else:
-            print("None of the numbers in your input match.")
+    guess_counter += 1
+    my_guess = int(input('Please Enter your Guess : '))
 
-        n = int(input("Enter your next guess: "))
+    if my_guess == number_to_guess:
+        print(f'The number is {number_to_guess} and you found it right !! in the {guess_counter} attempt')
+        break
 
-    ctr += 1
-    print(f"You've become a Mastermind! It took you {ctr} tries.")
+    elif guess_counter >= chances and my_guess != number_to_guess:
+        print(f'Oops sorry, The number is {number_to_guess} better luck next time')
+
+    elif my_guess > number_to_guess:
+        print('Your guess is higher ')
+
+    elif my_guess < number_to_guess:
+        print('Your guess is lesser')
